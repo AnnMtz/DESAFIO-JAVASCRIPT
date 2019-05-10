291aa41e70a0569becd5c183efe46e8f0705902d
anaMtz <amonmtz009@gmail.com>
2019-05-09 22:27:37 -0500

    agregando modal

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-// var postObject = {
-//   'name': 'Gogdo',
-//   'breed': 'Black Cat'
-// }
-// $('body').load("index.html");
-
-// $("#submit-button").click(()=>{
-//   let name = $("#pet-name").val();
-//   let breed = $("#pet-breed").val();
-//   let age = $("#pet-age").val();
-
-//   let petObject = {name,breed,age};
-//   console.log(petObject)
-//   addPet(petObject)
-  
-// })
-
-/*método post*/
+var pet ="";
+//método post
 const addPet = (petObject)=>{
   $.post( "https://jquerycrud-ed8dc.firebaseio.com/ana.json", /*URL*/
     JSON.stringify(petObject), /*Objeto a postear*/
@@ -24,7 +8,7 @@ const addPet = (petObject)=>{
   }, "json");
 }
 
-/*método get*/
+//método get
 function getData(){
   console.log("getting data")
   $.ajax({
@@ -36,7 +20,7 @@ function getData(){
   });
 }
 
-/*Método Update*/
+//Método Update
 function updateData(updatedObject){
     $.ajax({
        url: 'https://jquerycrud-ed8dc.firebaseio.com/ana/-LeK6izE4DBMIBDffGFY.json',
@@ -48,7 +32,7 @@ function updateData(updatedObject){
     });
 }
 
-/*Método delete*/
+//Método delete
 function deleteData(id){
   $.ajax({
      url: `https://jquerycrud-ed8dc.firebaseio.com/ana/${id}.json`,
@@ -68,37 +52,57 @@ function printData(dataToPrint) {
         `
         <div class="col col-sm-6 col-md-4 mb-4">
           <div class="card">
-              <img src="img/loki.jpj" class="card-img-top" alt="...">
+              <img src="img/perrito-saludando.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                   <h5 class="card-title">${value.name}</h5>
                   <p class="card-text">${value.description}</p>
-                  <a href="#" class="btn btn-primary">¡Adoptar!</a>
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">¡Adoptar!</button>
+                  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">Adoption information</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <form>
+                            <div class="form-group">Escribe tu nombre
+                              <label for="recipient-name" class="col-form-label"></label>
+                              <input type="text" class="form-control" id="recipient-name">
+                            </div>
+                            <div class="form-group">Escribe tu dirección
+                              <label for="recipient-address" class="col-form-label"></label>
+                              <input type="text" class="form-control" id="recipient-address">
+                            </div>
+                            <div class="form-group">¿Por qué quieres adoptarlo?
+                              <label for="recipient-question" class="col-form-label"></label>
+                              <input type="text" class="form-control" id="recipient-question">
+                            </div>
+                            <div class="form-group">¿Has tenido mascotas en casa?
+                              <label for="recipient-question-1" class="col-form-label"></label>
+                              <input type="text" class="form-control" id="recipient-question-1">
+                          </div>
+                          <div class="form-group">¿Quien se va a encargar de la mascota?
+                            <label for="recipient-question-2" class="col-form-label"></label>
+                            <input type="text" class="form-control" id="recipient-question-2">
+                          </div>
+                          </form>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Send message</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
               </div>
           </div>
       </div>`
       )
   })
 }
-
-// const loadContent = (file) => {
-//   // $('.container-wrapper').load(file)
-//   if(file == 'adoption.html') {
-//     $('.container-wrapper').load(file, function(){
-//       getData()
-//     })
-//   } else if (file == 'register.html') {
-//     $('.container-wrapper').load(file, function(){
-//     $("#submit-button").click(()=>{
-//       let name = $("#pet-name").val();
-//       let breed = $("#pet-breed").val();
-//       let age = $("#pet-age").val();
-    
-//       let petObject = {name,breed,age};
-//       console.log(petObject)
-//       addPet(petObject) 
-//     })
-//   })
-//   }
 
 const loadContent = (file) => {
   if(file == 'views/register.html') {
@@ -121,16 +125,3 @@ const loadContent = (file) => {
     })
   }
 }
-    //function(){
-  //   setTimeout(function(){
-  //     getData()
-  //   },2000)
-  // })
-//}
-/*
-const loadContent = (file) => {
-  $('.container-wrapper').load(file,function(){
-    setTimeout(function(){
-      getData()
-    },1000)
-})*/
