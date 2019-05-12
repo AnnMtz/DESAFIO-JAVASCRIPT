8ee3cf3e2ddc9aa7f78ae79bf2845f97c1a31dd2
anaMtz <amonmtz009@gmail.com>
2019-05-12 12:16:05 -0500

    subiendo validación

--- js/main.js
+++ js/main.js
@@ -50,9 +50,9 @@ function printData(dataToPrint) {
     console.log(`key ${key}, value ${value}, name ${value.name}, breed: ${value.breed}`)
     $(".data-wrapper").append(
         `
-        <div class="col col-sm-6 col-md-4 mb-4">
+        <div class="col col-sm-6 col-md-4 mb-4" data-id="${key}">
           <div class="card">
-              <img src="img/perrito-saludando.jpg" class="card-img-top" alt="...">
+            <img src="img/perrito-saludando.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                   <h5 class="card-title">${value.name}</h5>
                   <p class="card-text">${value.description}</p>
@@ -76,32 +76,41 @@ function printData(dataToPrint) {
                               <label for="recipient-address" class="col-form-label"></label>
                               <input type="text" class="form-control" id="recipient-address">
                             </div>
-                            <div class="form-group">¿Por qué quieres adoptarlo?
-                              <label for="recipient-question" class="col-form-label"></label>
-                              <input type="text" class="form-control" id="recipient-question">
+                            <div class="form-group">¿Cual es tu número telefonico?
+                              <label for="recipient-phone" class="col-form-label"></label>
+                              <input type="number" class="form-control" id="recipient-number">
                             </div>
-                            <div class="form-group">¿Has tenido mascotas en casa?
-                              <label for="recipient-question-1" class="col-form-label"></label>
-                              <input type="text" class="form-control" id="recipient-question-1">
+                            <div class="form-group">Ingresa tu email
+                              <label for="recipient-email" class="col-form-label"></label>
+                              <input type="text" class="form-control" id="recipient-email">
                           </div>
-                          <div class="form-group">¿Quien se va a encargar de la mascota?
-                            <label for="recipient-question-2" class="col-form-label"></label>
-                            <input type="text" class="form-control" id="recipient-question-2">
+                          <div class="form-group">¿Has tenido mascotas en casa?
+                            <label for="recipient-question" class="col-form-label"></label>
+                            <input type="text" class="form-control" id="recipient-question">
                           </div>
                           </form>
                         </div>
                         <div class="modal-footer">
                           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                          <button type="button" class="btn btn-primary">Send message</button>
+                          <button type="button" id="adopted" class="btn btn-primary">Send message</button>
                         </div>
                       </div>
                     </div>
-                  </div>
+		              </div>
               </div>
           </div>
       </div>`
       )
   })
+  $('#adopted').click((event) => {
+    getData()
+    console.log('adios');
+    let id =$(event.target).parent().data('id');
+    deleteData(id);
+    $(event.target).parent().fadeOut("fast", ()=> {});
+    console.log('adios');
+    
+  });
 }
 
 const loadContent = (file) => {
@@ -124,4 +133,96 @@ const loadContent = (file) => {
       getData()
     })
   }
+}
+
+function validateInput(forminput) {
+  $(forminput).removeClass("border-danger")
+  $(forminput).removeClass("border-succes")
+  if ($(forminput).val() === "") {
+    $(forminput).addClass("border-danger")
+    $("<p class='text-danger error animated fadeInLeft faster'>No dejes espacios en blanco</p>").insertAfter(forminput);
+    return false;
+  } else {
+    $(forminput).addClass("border-success")
+    return $(forminput).val()
+  }
+}
+
+function validateForm() {
+  $(".error").remove()
+  let name = validateInput("#pet-name");
+  let description = validateInput("#pet-description");
+  let image = validateInput("#pet-image");
+  if (name && description && image) {
+
+    let petObject = {
+      name,
+      description,
+      image
+    };
+    return petObject
+  } else {
+    return false
+  }
+}
+
+function agregar() {
+let result = validateForm();
+if (!result) {
+  $("#submit").removeClass("animated shake faster").width('100%').addClass("animated shake faster")
+} else {
+  $("#submit").fadeOut();
+  $("form").append('<div class="alert text-center alert-success  animated bounceInDown" role="alert">Gracias!!</div>')
+  postData(result)
+  setTimeout(()=> {
+    loadContent("./views/adoption.html")
+  },2000);
+}
+}
+
+
+function validateAdoptionForm() {
+  $(".error").remove()
+  let name = validateInput("#recipient-name");
+  let address = validateInput("#recipient-address");
+  let phone = validateInput("#recipient-phone");
+  let email = validateInput("#recipient-email")
+  let question = validateInput("#recipient-question")
+  if (name && address && phone && email && question) {
+    let adoptantObject = {
+      name,
+      address,
+      phone,
+      email,
+      question
+    };
+    return adoptantObject
+  } else {
+    return false
+  }
+}
+
+function adoptar() {
+  let result = validateAdoptionForm();
+  if (!result) {
+    $("#adopted").removeClass("animated shake faster").width('100%').addClass("animated shake faster")
+  } else {
+    $("#adopted").fadeOut();
+    $("form").append()('<div class="alert text-center alert-success  animated bounceInDown" role="alert">Gracias!!</div>')
+    postData(result)
+    setTimeout(()=> {
+      // loadContent("./views/adoption.html")
+    deleteData(petid,afterDelete);
+  },1000);
+}
+}
+
+function afterDelete() {
+  $("form").append(`
+  <div class="alert text-center alert-success animated bounceInDown" role="alert">Gracias por darme un nuevo hogar!
+  <span class="animated infinite zoomIn heartBeat delay-2s">&hearts;</span><br>
+  Nos pondremos en contacto contigo<br>
+  <a href="javascript:" onclick="loadHTML('./views/adoption.html');">Ver mas opciones</a>
+  </div>`
+  )
 }
